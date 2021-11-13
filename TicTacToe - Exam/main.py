63feea9ea7c545c6bc63a1eee5116fcf19caff74
Evelin Székely <evelin.szekely1@ibm.com>
2021-11-13 11:36:21 +0100

    red - set_up_board()

--- TicTacToe - Exam/main.py
+++ TicTacToe - Exam/main.py
@@ -1,2 +1,6 @@
 def init_board():
     return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+def set_up_board(input_board):
+    return True
--- TicTacToe - Exam/test.py
+++ TicTacToe - Exam/test.py
@@ -10,3 +10,14 @@ class TestClass(unittest.TestCase):
 
     def test_init_board_negative(self):
         self.assertNotEqual([[1, 0, 0], [0, 0, 0], [0, 0, 0]], main.init_board())
+
+    def test_set_up_board(self):
+        input_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        output_board = """
+    0|0|0
+    -+-+-
+    0|0|0
+    -+-+-
+    0|0|0
+    """
+        self.assertEqual(output_board, main.set_up_board(input_board))
